fe34dd423472b5493f5f397f90dbae7e97e1fd9b
m10653 <mcombsb1@gmail.com>
2016-05-09 16:15:47 -0400

    Tried to Convert from maven project.
    Removed multi try catch, To change from java 8 change the lib import

--- JARVIS/src/net/jarvis/io/Speaker.java
+++ JARVIS/src/net/jarvis/io/Speaker.java
@@ -1,13 +1,10 @@
 package net.jarvis.io;
 
 import java.io.File;
-import java.io.IOException;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class Speaker {
 	
@@ -24,7 +21,7 @@ public class Speaker {
 			clip.start();
 			stream.close();
 			System.gc(); //here becuse of java bug;
-		} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("Error in Speaker Class");
 		}
